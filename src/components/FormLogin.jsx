e733c85dc615c2033bbb18da6cf3afe3eb242b49
Yael Ruiz Pucheta <yaelruiz99@outlook.com>
2022-12-08 12:03:39 -0600

    Set Values and Reset Form

--- src/components/FormLogin.jsx
+++ src/components/FormLogin.jsx
@@ -1,33 +1,51 @@
 import { useFormik } from 'formik';
-import React from 'react';
+import * as Yup from 'yup';
+
+const schema = Yup.object().shape({
+  email: Yup.string().email('Email invalido').required(),
+  password: Yup.string().min(1).required(),
+});
 
 export const FormLogin = () => {
   const submitForm = (values) => {};
 
-  const { handleSubmit, handleChange } = useFormik({
-    initialValues: {
-      email: '',
-      password: '',
+  const { handleSubmit, handleChange, handleReset, errors, values } = useFormik(
+    {
+      initialValues: {
+        email: '',
+        password: '',
+      },
+      onSubmit: submitForm,
+      validationSchema: schema,
     },
-    onSubmit: submitForm,
-  });
+  );
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} onReset={handleReset}>
         <input
           type='email'
           placeholder='email'
           name='email'
+          value={values.email}
           onChange={handleChange}
+          autoComplete='off'
         />
         <input
           type='password'
           placeholder='password'
           name='password'
+          value={values.password}
           onChange={handleChange}
+          autoComplete='off'
         />
         <button type='submit'>iniciar sesión</button>
+        <button type='reset'>Limpiar</button>
+        <br />
+        {errors.email && <span>Email invalido</span>}
+        {errors.password && (
+          <span>La contraseña debe tener al menos 1 caracter</span>
+        )}
       </form>
     </div>
   );
